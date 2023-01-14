bdfa79345434050d75ef0ac52c65dc82a59905d6
Aditi Acharya <alishaacharya77@gmail.com>
2023-01-14 20:15:37 +0545

    displayed location name on hovering the marker

--- src/map/markers.tsx
+++ src/map/markers.tsx
@@ -1,13 +1,18 @@
-import {  Marker, Popup} from 'react-leaflet';
+import { Marker, Popup } from "react-leaflet";
 
-
-export default function LeafletMarker(props : { location: {lat: number, lon :number}, displayName: string }) {
-    const {location, displayName} = props;
-    return (
-        <Marker position={{lat: location.lat, lng: location.lon}}>
-            <Popup>
-                {displayName}
-            </Popup>
-        </Marker>
-    );
+export default function LeafletMarker(props: {
+  location: { lat: number; lon: number };
+  displayName: string;
+}) {
+  const { location, displayName } = props;
+  return (
+    <Marker
+      position={{ lat: location.lat, lng: location.lon }}
+      eventHandlers={{
+        mouseover: (event) => event.target.openPopup(),
+      }}
+    >
+      <Popup>{displayName}</Popup>
+    </Marker>
+  );
 }
